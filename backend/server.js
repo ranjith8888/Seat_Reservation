e60f23c3267de990b6e3627c7fda312f022079c8
ranjith8888 <goudp1234@gmail.com>
2024-12-30 21:08:06 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3002",
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
